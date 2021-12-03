ccee5380c868c2265384fb9f49f73f879744710e
aytacguley <aytacguley@dklassix.com>
2021-12-03 21:58:03 +0200

    Call To Action component completed

--- src/components/callToAction/CallToAction.jsx
+++ src/components/callToAction/CallToAction.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './CallToAction.css';
 
-const CallToAction = () => {
-  return <div>Call To Action</div>;
-};
+const CallToAction = () => (
+  <div className='gpt3__cta'>
+    <div className='gpt3__cta-content'>
+      <p>Request Early Access to Get Started</p>
+      <h3>Register Today &#38; start exploring the endless possibilities.</h3>
+    </div>
+    <div className='gpt3__cta-btn'>
+      <button type='button'>Get Started</button>
+    </div>
+  </div>
+);
 
 export default CallToAction;
