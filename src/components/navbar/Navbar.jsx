b21b2870a9c9b7841ec0aa67f2628e9a14acb9a1
aytacguley <aytacguley@dklassix.com>
2021-12-03 20:49:16 +0200

    Navbar component created and completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NavLinks from './NavLinks/NavLinks';
 import './Navbar.css';
+import MobileMenu from './MobileMenu/MobileMenu';
+import NavLogo from './NavLogo/NavLogo';
+
+//BEM -> Block Element Modifier
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <NavLogo />
+        <div className='gpt3__navbar-links_container'>
+          <NavLinks />
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>
+      <MobileMenu toggleMenu={toggleMenu} setToggleMenu={setToggleMenu} />
+    </div>
+  );
 };
 
 export default Navbar;
